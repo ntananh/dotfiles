ffa5374e86193b5c087775c49575dc5ff9678b96
Nguyen Tan Anh <tananh691@gmail.com>
2022-11-18 10:59:05 +0700

    fix[shell]: fix set-up script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh 
 DOTFILE_DIR=$(pwd)
 
-ln -sfn $DOTFILE_DIR/{alacritty,nvim} $HOME/.config
+ln -sfn $DOTFILE_DIR/alacritty $HOME/.config/alacritty
+ln -sfn $DOTFILE_DIR/nvim $HOME/.config/nvim
 ln -sfn $DOTFILE_DIR/tmux/.tmux.conf $HOME/.tmux.conf
-# ln -sfn $DOTFILE_DIR/.zshrc $HOME/.zshrc
+ln -sfn $DOTFILE_DIR/shell/.zshrc $HOME/.zshrc
