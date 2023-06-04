ff9e490fd12913747d24450db5ada1b83850c0e8
Nguyen Tan Anh <tananh691@gmail.com>
2023-06-04 23:04:06 +0700

    docs[setup]: add more setup notes

--- setup.sh
+++ setup.sh
@@ -5,5 +5,8 @@ ln -sfn $DOTFILE_DIR/alacritty $HOME/.config/alacritty
 ln -sfn $DOTFILE_DIR/nvim $HOME/.config/nvim
 ln -sfn $DOTFILE_DIR/tmux/.tmux.conf $HOME/.tmux.conf
 ln -sfn $DOTFILE_DIR/shell/.zshrc $HOME/.zshrc
+ln -sfn $DOTFILE_DIR/yabai $HOME/.config/yabai 
+ln -sfn $DOTFILE_DIR/skhd $HOME/.config/skhd 
+ln -sfn $DOTFILE_DIR/sketchybar $HOME/.config/sketchybar 
 ln -sfn /usr/bin/batcat /usr/local/bin/bat
 ln -sfn /usr/bin/fdfind /usr/local/bin/fd
